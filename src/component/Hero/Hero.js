0fa6dc66b574afefc7a9886458dfb13564cffec9
HARSH TRIPATHI <tripathihk2014@gmail.com>
2022-01-13 23:10:54 +0530

    Line break issue in hero section

--- src/component/Hero/Hero.js
+++ src/component/Hero/Hero.js
@@ -35,7 +35,7 @@ const Hero = () => {
                                     </p>
                                 </ScrollAnimation>
 
-                            </div>
+                            </div> 
                         </div>
                     </div>
                 </div>
